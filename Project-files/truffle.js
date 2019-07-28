b01c029419dfdec8687068a30fad9672b3aa6cac
grajnikanth <grajnikanth@gmail.com>
2019-07-28 15:49:00 -0700

    Blockchain Supply Chain

---
+++ Project-files/truffle.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const infuraKey = "7eb245842fcb41d895dd286cb02546c1";
+
+const mnemonic = "buyer curve festival family seminar submit margin moment blouse dish pistol slow";
+
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+
+      // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    },
+  }
+};
+
